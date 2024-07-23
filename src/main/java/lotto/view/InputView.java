94f46a4664d18f52a973b544e4af319bcd2afd68
dlgustj206 <dlgustj206@naver.com>
2024-07-23 17:44:07 +0900

    refactor: `inputWinningNumber` 메서드 반환 타입 변경 및 숫자 목록 처리 로직 추가
    - 기존의 `inputWinningNumber` 메서드가 String을 반환하던 것을 List<Integer>를 반환하도록 변경함.
    - 새로운 `numberList` 메서드를 추가하여 입력된 문자열을 숫자 목록으로 변환하도록 함.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,20 +2,33 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
+
 
     public String inputBuyAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
-    public String inputWinningNumber() {
+    public List<Integer> inputWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        return Console.readLine();
+        return numberList(Console.readLine());
     }
 
     public int inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         return Integer.parseInt(Console.readLine());
+    }
+
+    public List<Integer> numberList(String input) {
+        String[] numbers = input.split(",");
+        List<Integer> winningNumberList = new ArrayList<Integer>();
+        for (String number : numbers) {
+            winningNumberList.add(Integer.parseInt(number));
+        }
+        return winningNumberList;
     }
 }
