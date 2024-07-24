ca634c413f002e6a7c5f0f618eb650d48fc960c3
이현서 <dlgustj206@naver.com>
2024-07-24 12:56:52 +0900

    feat: 로또 번호 중복 검증 추가
    - Lotto 클래스의 validate 메서드에 번호 중복 검증 로직을 추가함.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import lotto.utils.exception.DuplicatedNumberException;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,11 +14,26 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getLottoNumbers() {
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (isDuplicatedNumber(numbers)) {
+            throw new DuplicatedNumberException();
+        }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isDuplicatedNumber(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (Integer number : numbers) {
+            if (!set.add(number)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
