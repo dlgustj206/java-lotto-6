4c46c66233f775487177bc396c15cb1eee211366
이현서 <dlgustj206@naver.com>
2024-07-24 12:49:30 +0900

    feat: LottoNumberList 클래스 추가 및 로또 번호 생성 기능 구현

---
+++ src/main/java/lotto/domain/LottoNumberList.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoNumberList {
+
+    public static List<Integer> lottoNumbers;
+
+    public static List<Integer> generateLottoNumbers() {
+        lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoNumberList = new ArrayList<>(lottoNumbers);
+        Collections.sort(lottoNumberList);
+        return lottoNumberList;
+    }
+}
