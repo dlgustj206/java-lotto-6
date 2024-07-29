b1e13c65a6032d2c5e1aa948ec8bf3187c27c2ca
이현서 <dlgustj206@naver.com>
2024-07-29 15:54:31 +0900

    chore: 로또 번호 범위에 관한 에러 메시지 내용 수정

--- src/main/java/lotto/utils/exception/OutOfRangeNumberException.java
+++ src/main/java/lotto/utils/exception/OutOfRangeNumberException.java
@@ -2,6 +2,6 @@ package lotto.utils.exception;
 
 public class OutOfRangeNumberException extends IllegalArgumentException{
     public OutOfRangeNumberException() {
-        super("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
+        super("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 }
