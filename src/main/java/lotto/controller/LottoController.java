775376e05115cc9fac7ae8604d01f3621a74ed37
이현서 <dlgustj206@naver.com>
2024-07-29 15:30:41 +0900

    feat: LottoController 클래스 생성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,16 @@
+package lotto.controller;
+
+import lotto.utils.InputValidator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+}
