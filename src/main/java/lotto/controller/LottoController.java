84cdc2e6c9982109abea54e58d47e528f5e46d16
이현서 <dlgustj206@naver.com>
2024-07-29 15:51:04 +0900

    feat: LottoController 클래스에 run 및 initBuyAmount 메서드 추가
    - 로또 프로그램의 주요 로직을 담당하는 메서드를 추가하여, 사용자 입력 처리와 로또 티켓 구매 계산을 위한 기능을 구현함.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,7 @@
 package lotto.controller;
 
 import lotto.utils.InputValidator;
+import lotto.utils.exception.AmountNotDivisibleByThousandException;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -12,5 +13,24 @@ public class LottoController {
     public LottoController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+    }
+
+    public void run() {
+        Integer amount = initBuyAmount();
+        Integer ticketCount = amount / 1000;
+        outputView.printBuyAmount(ticketCount);
+    }
+
+    public Integer initBuyAmount() {
+        while (true) {
+            try {
+                String amount = inputView.inputBuyAmount();
+                Integer initAmount = Integer.valueOf(amount);
+                InputValidator.amountValidator(initAmount);
+                return initAmount;
+            } catch (AmountNotDivisibleByThousandException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
     }
 }
